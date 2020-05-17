dfdb0541277c961428c54b1e3e6696c1c916ae83
madolphe <madolphe@ensc.fr>
2020-05-17 12:45:10 +0200

    override tostr for population class + fix bug on mutation rate + add same seed for new exp

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# @TODO: write history function for each agent
+# @TODO: print reward history
+# @TODO: redefined environment variable
+
 import gym
 import numpy as np
 
@@ -10,13 +14,16 @@ class Agent:
         self.reward = 0
         self.weights_mutation_rate = mutation_rate
 
-    def get_random_weight_matrix(self):
-        return np.array([np.random.normal(0.5, 0.1) for _ in range(4)])
+    @staticmethod
+    def get_random_weight_matrix():
+        return np.array([np.random.normal(0.5, 0.001) for _ in range(4)])
 
     def mutate(self):
         nb_to_update = int(np.round(self.weights.shape[0] * self.weights_mutation_rate))
         for _ in range(nb_to_update):
-            self.weights[np.random.randint(0, len(self.weights))] *= (0.2*np.random.random() - 0.1)
+            mutation_rate = 0.02*np.random.random() - 0.01
+            rdm_idx = np.random.randint(0, len(self.weights))
+            self.weights[rdm_idx] *= (1+mutation_rate)
 
     def action(self, observations):
         logit = np.dot(self.weights, observations)
@@ -25,17 +32,29 @@ class Agent:
             return 0
         return 1
 
+    def history(self):
+        pass
+
 
 class Population:
     def __init__(self, env, mutation_rate, population_size):
         self.agents = [Agent(self.sigmoid, 0.5, 1) for _ in range(population_size)]
         self.env = env
-        self.mutation_rate = mutation_rate
+        self.nb_mutation = int(np.round(mutation_rate * population_size))
         self.population_size = population_size
 
-    def get_reward_on_gen(self):
+    def __str__(self):
+        description = ''
+        for index, agent in enumerate(self.agents):
+            description += 'Agent ' + str(index) + ': '
+            description += str(agent.weights) + ' | '
+            description += str(agent.reward) + ' \n '
+        return description
+
+    def get_reward_on_gen(self, seed):
         for agent in self.agents:
             agent.reward = 0
+            self.env.seed(seed)
             init_obs = self.env.reset()
             done = False
             next_action = agent.action(init_obs)
@@ -46,7 +65,8 @@ class Population:
 
     def update_gen(self):
         self.order_agents()
-        self.agents = self.agents[:round(len(self.agents)*self.mutation_rate)]
+        self.agents = self.agents[:self.nb_mutation]
+        print("{} agents are kept to be mutated".format(str(self.nb_mutation)))
         for agent in self.agents:
             agent.mutate()
         for i in range(self.population_size - len(self.agents)):
@@ -58,7 +78,6 @@ class Population:
         pass
 
     def show_best_agent(self):
-        print(self.agents[0].reward)
         init_obs = self.env.reset()
         next_action = self.agents[0].action(init_obs)
         done = False
@@ -75,11 +94,13 @@ class Population:
 if __name__ == '__main__':
     nb_epoch = 20
     env = gym.make('CartPole-v1')
-    pop = Population(env, 0.5, 1)
+    pop = Population(env, 0.5, 5)
+    seed = np.random.randint(1)
     for _ in range(nb_epoch):
         print("epoch number: ", _)
-        pop.get_reward_on_gen()
+        pop.get_reward_on_gen(seed)
         pop.show_best_agent()
         pop.update_gen()
+        print(pop)
     env.close()
 
